905c50a4e3ec09c8b55e213f81669a1d889e647b
volleyballfantic2 <75176232+volleyballfantic2@users.noreply.github.com>
2020-12-01 13:43:36 -0500

    Add project files.

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.models.Movie> Movie { get; set; }
+    }
+}
---
+++ models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.models
+{
+    public class Movie
+    {
+        public int ID { get; set; } //Primary Key
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
